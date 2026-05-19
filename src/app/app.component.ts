6329eae310dc87db7615212fc2ceddaf0f217b73
aniadewor <116655947+aniadewor@users.noreply.github.com>
2026-05-19 19:55:10 +0200

    add main component view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BanersComponent } from './baners/baners.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, NavbarComponent,BanersComponent, CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/baners/baners.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baners',
+  imports: [],
+  templateUrl: './baners.component.html',
+  styleUrl: './baners.component.css'
+})
+export class BanersComponent {
+
+}
